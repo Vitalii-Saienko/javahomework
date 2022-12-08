6954930910725d77efb139388b27af372769a5b4
Vitalii-Saienko <vitaliy5saie@gmail.com>
2022-12-08 18:13:36 +0100

    for task1

---
+++ src/Main.java
@@ -0,0 +1,20 @@
+public class Main {
+    public static void main(String[] args) {
+        byte baitik = -127;
+        System.out.println(baitik);
+        short shortik = 255;
+        System.out.println(shortik);
+        int intik = 3000;
+        System.out.println(intik);
+        long longer = 1000L;
+        System.out.println(longer);
+        float flot = 25f;
+        System.out.println(flot);
+        double bubble = 2.6;
+        System.out.println(bubble);
+        boolean ishard = true;
+        System.out.println(ishard);
+        char charik = 'A';
+        System.out.println(charik);
+    }
+}
